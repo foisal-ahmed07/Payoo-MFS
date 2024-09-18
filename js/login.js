ad54b21b801702b575eef4c2477495e121cf2472
Foisal Ahmed <shakhfoisal99@gmail.com>
2024-09-18 20:33:47 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,25 @@
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    // step2: prevent the default behavior
+    event.preventDefault();
+    // step3: get the phone number and pin number
+    const phoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById("number").value;
+    console.log(phoneNumber, pinNumber);
+    // validate phone and pin
+    if(phoneNumber === "5" && pinNumber  === "1234"){
+        console.log("Your are logged in");
+        window.location.href = "../home.html"
+    }
+    else{
+        alert("wrong phone number or pin number")
+    }
+
+
+
+
+
+
+
+  });
