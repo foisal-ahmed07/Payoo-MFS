d1f8e45257534810cd72e76333beb8ef0f8687eb
Foisal Ahmed <shakhfoisal99@gmail.com>
2024-09-18 23:26:35 +0600

    repair done

--- js/home.js
+++ js/home.js
@@ -7,21 +7,29 @@
 document
   .getElementById("add-money-btn")
   .addEventListener("click", function (event) {
+
+    //  reload prevent 
     event.preventDefault();
 
     const inputMoney = document.getElementById("input-money-amount").value;
 
     const pinNumber = document.getElementById("pin-number").value;
 
+    // pin number condition
     if (pinNumber === "1234") {
+
+        // previous amount value 
       const previousAmount =
         document.getElementById("previous-amount").innerText;
 
+        // convert to integer/float number
       const addMoney = parseFloat(inputMoney);
       const previousMoney = parseFloat(previousAmount);
 
+    //   sum the newAmount = inputAmount + previousAmount
       let newAmount = addMoney + previousMoney;
 
+    //   update previous amount
       document.getElementById("previous-amount").innerText = newAmount;
     } else {
       alert("Wrong Information! Please try again");
